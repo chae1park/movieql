c27099f0fbff37033e7bd94150913d97b80f998f
chaewon park <chaewonpark@g.skku.edu>
2021-03-07 22:17:18 +0900

    added fetch

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,43 +1,16 @@
-export let movies = [
-    {
-        id:1,
-        name: "Avengers",
-        score: 8
-    },
-    {
-        id:2,
-        name: "Logan",
-        score: 2
-    },
-    {
-        id:3,
-        name: "First Class",
-        score: 10
+import fetch from "node-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json";
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit>0){
+        REQUEST_URL += `limit=${limit}`;
+
     }
-]
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-
-  }
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length> cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+    if (rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
-  };
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: Number(`${movies.length+1}`),
-        name,
-        score
-    };
-    movies.push(newMovie)
-    return newMovie
+    return fetch(`${API_URL}`)
+      .then(res => res.json())
+      .then(json => json.data.movies);
 }
